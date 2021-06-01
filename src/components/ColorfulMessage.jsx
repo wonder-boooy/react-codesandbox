d641e4abba002ccd5ad6effef6d5cfe72ace78a0
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 11:30:40 +0900

    AnotherNotation

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
+
   const contentStyle = {
-    color: props.color,
+    color: color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
