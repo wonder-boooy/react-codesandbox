6e9f8f6484ee3a96db9a929281534b952e1e08c9
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 12:14:19 +0900

    BeFoolWhenMultipleOf3

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,17 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickCountUp = () => setNum(num + 1);
   const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickCountUp = () => setNum(num + 1);
+  const onClickSwitchShowFlag = () => setFaceShowFlag(!faceShowFlag);
+
+  if (num % 3 === 0) {
+    faceShowFlag || setFaceShowFlag(true);
+  } else {
+    faceShowFlag && setFaceShowFlag(false);
+  }
 
   return (
     <>
@@ -11,7 +20,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(σ´∀｀)σ ファ〜ン!!</p>}
     </>
   );
 };
