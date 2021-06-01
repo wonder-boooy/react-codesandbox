4a507cd833ab76a445c3859877ca7e1ca349f7ea
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 11:04:30 +0900

    CreateFirstPage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return <h1>こんにちは</h1>;
+  return (
+    <>
+      <h1>こんにちは</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
