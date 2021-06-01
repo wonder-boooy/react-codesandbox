6a0986bd1fee4046ef9bd2f4859eaa34c8713b34
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 11:27:46 +0900

    UseComponent

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const App = () => {
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
-      <ColorfulMessage />
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ColorfulMessage = () => {
+const ColorfulMessage = (props) => {
   const contentStyle = {
-    color: "blue",
+    color: props.color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>お元気ですか？</p>;
+  return <p style={contentStyle}>{props.children}</p>;
 };
 
 export default ColorfulMessage;
