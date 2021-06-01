137564f3c9827026ec741f35095cfc94f7a4baf2
yuma wada <yuma.wada.1127@gmail.com>
2021-06-01 11:22:31 +0900

    SeparateToComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const onClickButton = () => alert("ボタンクリック！");
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <ColorfulMessage />
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ColorfulMessage = () => {
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+
+  return <p style={contentStyle}>お元気ですか？</p>;
+};
+
+export default ColorfulMessage;
